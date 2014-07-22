bf8ef9bf51197ddedfc5549969c2f043d0edab9e
Tanner Steward & J.J. Idt <student@epicodus.com>
2014-07-22 16:55:12 -0700

    poker hand first commit

--- lib/poker_hand.rb
+++ lib/poker_hand.rb
@@ -1,6 +1,6 @@
 
 def poker_hand(hand)
-  card_value = {'1' => 1,
+  card_value = {'1' => 10,
                 '2' => 2,
                 '3' => 3,
                 '4' => 4,
@@ -16,11 +16,24 @@ def poker_hand(hand)
                 'A' => 14}
 
   number_cards = []
+  suit_cards = []
   hand.each do |i|
     number_cards << i.split("")[0]
+    suit_cards << i.split("")[-1]
   end
 
   duplicates = Hash.new(0)
+
+  number_cards.map! do |i|
+    card_value[i]
+  end
+
+  number_cards.sort!
+
+  starting_card = number_cards[0].to_i
+
+  comparison_cards = [starting_card, (starting_card+1), (starting_card+2), (starting_card+3), (starting_card+4)]
+
 
   number_cards.each do |element|
     duplicates[element] += 1
@@ -28,9 +41,17 @@ def poker_hand(hand)
 
   if duplicates.length ==3 && duplicates.has_value?(3)
     "3 of a kind"
+  elsif duplicates.length ==2 && duplicates.has_value?(4)
+    "4 of a kind"
+  elsif duplicates.length ==2 && duplicates.has_value?(3) && duplicates.has_value?(2)
+    "full house"
   elsif duplicates.length ==4
     "1 pair"
   elsif duplicates.length ==3
     "2 pair"
+  elsif number_cards == comparison_cards
+    "straight"
+  elsif suit_cards.uniq.length ==1
+    "flush"
   end
 end
--- spec/poker_hand_spec.rb
+++ spec/poker_hand_spec.rb
@@ -3,7 +3,7 @@ require('poker_hand')
 
 describe("poker_hand") do
   it("takes a poker hand with two of the same numbered cards and returns 1 pair") do
-    poker_hand(["3S", "2D", "7H", "8D", "2S"]).should(eql("1 pair"))
+    poker_hand(["3S", "2D", "7H", "8D", "2S"]).should(eq("1 pair"))
   end
 
   it("takes a poker hand with 2 cards in one rank and 2 other cards in another rank") do
@@ -12,5 +12,21 @@ describe("poker_hand") do
 
   it("takes a poker hand with 3 of a kind and returns '3 of a kind'") do
     poker_hand(["3S", "3D", "3H", "4D", "7H"]).should(eq("3 of a kind"))
+  end
+
+  it("takes a poker hand with 4 of a kind and returns '4 of a kind'") do
+    poker_hand(["2S", "2H", "2D", "2C", "5H"]).should(eq("4 of a kind"))
+  end
+
+  it("takes a poker hand with a pair and 3 of a kind and returns 'full house'") do
+    poker_hand(["2S", "2H", "2D", "5C", "5H"]).should(eq("full house"))
+  end
+
+  it("takes a poker hand with cards in numerical order and returns 'straight'") do
+    poker_hand(["9H", "10H", "JS", "QC", "KH"]).should(eq("straight"))
+  end
+
+  it("takes a poker hand with 5 cards of the same suit and returns 'flush'") do
+    poker_hand(["2H", "4H", "7H", "JH", "10H"]).should(eq("flush"))
   end
 end
